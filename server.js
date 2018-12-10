361df9ab5b06393446d0119ff416372fdb8d90dd
Ilya Filippov <89099109104@mail.ru>
2018-12-10 18:53:16 +0300

    Rewrite server file

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const { ApolloServer } = require('apollo-server')
+const mongoose = require('mongoose')
+require('dotenv').config({ path: 'variables.env' })
+const fs = require('fs')
+const path = require('path')
 
-const typeDefs = require('./schema')
-const resolvers = require('./data/resolvers')
+const User = require('./models/User')
+const Post = require('./models/Post')
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log('DB connected.'))
+  .catch(err => console.error(err))
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers
+  typeDefs: fs.readFileSync(path.join(__dirname, 'typeDefs.gql'), 'utf-8'),
+  context: {
+    User,
+    Post
+  }
 })
 
 server.listen()
