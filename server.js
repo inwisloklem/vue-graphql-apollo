3e1abc9c2b79a5f28ac29f74093a11a6b8d5ab1d
Ilya Filippov <89099109104@mail.ru>
2018-12-11 18:35:46 +0300

    Refactor server

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
+const dotenv = require('dotenv')
 const fs = require('fs')
+const mongoose = require('mongoose')
 const path = require('path')
+const resolvers = require('./resolvers')
 const Post = require('./models/Post')
 const User = require('./models/User')
-const mongoose = require('mongoose')
 const { ApolloServer } = require('apollo-server')
 
-require('dotenv').config({ path: 'variables.env' })
-
+dotenv.config({ path: 'variables.env' })
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log('DB connected.'))
   .catch(err => console.error(err))
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, 'typeDefs.gql'), 'utf-8'),
-  context: {
-    User,
-    Post
-  }
+  resolvers,
+  context: { User, Post }
 })
 
 server.listen()
